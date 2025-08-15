bc860cff53515ea4373c4e42f24745dde828ef9d
Lukas Neverauskis <lukas.neverauskis@gmail.com>
2025-08-15 19:23:42 +0300

    refactor: move in toolchain files from m8ec

---
+++ cmake/toolchains/arm-none-eabi-gcc.cmake
@@ -0,0 +1,70 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR ARM)
+set(CMAKE_CROSSCOMPILING TRUE)
+
+set(toolchain_prefix "arm-none-eabi-")
+find_program(toolchain_gcc_path ${toolchain_prefix}gcc)
+if(NOT toolchain_gcc_path)
+  message(
+    FATAL_ERROR
+      "Could not find ${toolchain_prefix}gcc. Please ensure the ARM GCC toolchain is installed and in your PATH."
+  )
+endif()
+
+get_filename_component(arm_toolchain_dir ${toolchain_gcc_path} DIRECTORY)
+
+set(CMAKE_C_COMPILER ${arm_toolchain_dir}/${toolchain_prefix}gcc)
+set(CMAKE_CXX_COMPILER ${arm_toolchain_dir}/${toolchain_prefix}g++)
+set(CMAKE_ASM_COMPILER ${arm_toolchain_dir}/${toolchain_prefix}gcc)
+set(CMAKE_AR ${arm_toolchain_dir}/${toolchain_prefix}ar)
+set(CMAKE_OBJCOPY
+    ${arm_toolchain_dir}/${toolchain_prefix}objcopy
+    CACHE INTERNAL "objcopy tool"
+)
+set(CMAKE_OBJDUMP
+    ${arm_toolchain_dir}/${toolchain_prefix}objdump
+    CACHE INTERNAL "objdump tool"
+)
+set(CMAKE_NM
+    ${arm_toolchain_dir}/${toolchain_prefix}nm
+    CACHE INTERNAL "nm tool"
+)
+set(CMAKE_SIZE_UTIL
+    ${arm_toolchain_dir}/${toolchain_prefix}size
+    CACHE INTERNAL "size tool"
+)
+
+set(CMAKE_FIND_ROOT_PATH ${arm_toolchain_dir})
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+set(CMAKE_C_COMPILER_WORKS 1)
+set(CMAKE_CXX_COMPILER_WORKS 1)
+
+set(compiler_flags "-fstack-usage -fdata-sections -ffunction-sections -g")
+
+string(APPEND CMAKE_C_FLAGS " ${compiler_flags}")
+string(APPEND CMAKE_CXX_FLAGS
+       " ${compiler_flags} -fno-rtti -fno-exceptions -fno-threadsafe-statics"
+)
+string(APPEND CMAKE_ASM_FLAGS " ${compiler_flags} -x assembler-with-cpp")
+string(
+  APPEND
+  CMAKE_EXE_LINKER_FLAGS
+  " --specs=nosys.specs -static -Wl,--gc-sections -Wl,--print-memory-usage -u _printf_float -Wl,--start-group -lc -lm -lstdc++ -lsupc++ -Wl,--end-group"
+  # -Wl,--verbose -Wl,--trace
+)
+
+# https://gcc.gnu.org/onlinedocs/gcc/Optimize-Options.html
+# https://gcc.gnu.org/onlinedocs/gcc/Debugging-Options.html
+
+set(compiler_flags_debug " -gdwarf-2 -Og")
+set(CMAKE_C_FLAGS_DEBUG_INIT ${compiler_flags_debug})
+set(CMAKE_CXX_FLAGS_DEBUG_INIT ${compiler_flags_debug})
+set(CMAKE_ASM_FLAGS_DEBUG_INIT ${compiler_flags_debug})
+
+set(compiler_flags_release " -O1")
+set(CMAKE_C_FLAGS_RELEASE_INIT ${compiler_flags_release})
+set(CMAKE_CXX_FLAGS_RELEASE_INIT ${compiler_flags_release})
+set(CMAKE_ASM_FLAGS_RELEASE_INIT ${compiler_flags_release})
