84088439da83e15ee637938f746ce4ffb4b36ba6
Lukas Neverauskis <lukas.neverauskis@gmail.com>
2025-09-04 01:04:33 +0300

    refactor: move out Middlewares/Third_Party/FreeRTOS to extern/ln

--- cmake/toolchains/arm-none-eabi-gcc.STM32H750.cmake
+++
@@ -1,8 +0,0 @@
-include(${CMAKE_CURRENT_LIST_DIR}/arm-none-eabi-gcc.cmake)
-
-set(device_flags "-mcpu=cortex-m7 -mthumb -mfpu=fpv5-d16 -mfloat-abi=hard")
-
-string(APPEND CMAKE_C_FLAGS " ${device_flags}")
-string(APPEND CMAKE_CXX_FLAGS " ${device_flags}")
-string(APPEND CMAKE_ASM_FLAGS " ${device_flags}")
-string(APPEND CMAKE_EXE_LINKER_FLAGS " ${device_flags}")
